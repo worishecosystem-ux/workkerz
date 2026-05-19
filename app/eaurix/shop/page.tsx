cc58ae9e3bb02a699f4d48d6e1f545b536252587
RSW ECOSYSTEM PVT. LTD. <rswecosystempvt.ltd.@RSWs-Mac-mini.local>
2026-05-19 11:34:20 +0530

    update

--- app/eaurix/shop/page.tsx
+++ app/eaurix/shop/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { Suspense } from "react";
 import { EAurixShop } from "../components/EAurixShop";
 
-export default function Page() {
+function ShopContent() {
   return <EAurixShop />;
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ShopContent />
+    </Suspense>
+  );
 }
