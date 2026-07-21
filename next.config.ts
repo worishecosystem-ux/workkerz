2e07ce1e5d0ab64af635be0223419dbb48b4caaa
RSW ECOSYSTEM PVT. LTD. <rswecosystempvt.ltd.@RSWs-Mac-mini.local>
2026-07-21 17:19:01 +0530

    Your commit message

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["pdfkit"],
   devIndicators: false,
 
-  allowedDevOrigins: [
-    "localhost",
-    "127.0.0.1",
-    "172.29.22.14",
-  ],
+  
 };
 
 export default nextConfig;
